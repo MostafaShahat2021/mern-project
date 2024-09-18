1e4cd359ed48aa6ee0871a44f2b8b1e0df5beb96
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-09-18 15:58:52 +0300

    set type to module and install nodemon as dependencies

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,6 @@
-require('dotenv').config();
-const express = require('express');
+import 'dotenv/config'
+import express from 'express';
 const PORT = process.env.PORT || 3000;
 const app = express();
 
-app.listen(PORT, () => `Server up & running on port: ${PORT}`)
+app.listen(3000, () => console.log(`Server up & running on http://localhost:${PORT}`))
