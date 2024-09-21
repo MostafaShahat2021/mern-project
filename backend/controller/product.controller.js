ed4f6785d57502b0c610a85eb984fa6f1f112a09
MostafaShahat2021 <mostafa.shahat2014@gmail.com>
2024-09-21 21:11:56 +0300

    Add updateProduct controller

--- backend/controller/product.controller.js
+++ backend/controller/product.controller.js
@@ -1,3 +1,4 @@
+import mongoose from 'mongoose';
 import Product from '../model/product.model.js';
 export const addProduct = async (req, res) => {
   const {name, price, image } = req.body;
@@ -57,6 +58,30 @@ export const getProducts = async(req, res) => {
       data: products
     })
 
+  } catch (error) {
+    res.status(500).json({
+      success: false,
+      message: `Server error: ${error.message}`,
+    })
+  }
+}
+
+export const updateProduct = async(req, res) => {
+  const {id} = req.params
+  const product = req.body
+
+  if(!mongoose.Types.ObjectId.isValid(id)){
+    return res.status(404).json({
+      success: false,
+      message: `invaled product id : ${id}`
+    })
+  }
+  try {
+    const updatedProduct = await Product.findByIdAndUpdate(id, product, {new:true})
+    res.status(201).json({
+      success: true,
+      data: updatedProduct
+    })
   } catch (error) {
     res.status(500).json({
       success: false,
--- backend/routes/product.js
+++ backend/routes/product.js
@@ -1,9 +1,9 @@
 import express from 'express';
-import { addProduct, deleteProduct, getProducts } from '../controller/product.controller.js';
+import { addProduct, deleteProduct, getProducts, updateProduct } from '../controller/product.controller.js';
 const router = express.Router();
 
 router.route('/products').get(getProducts)
   .post(addProduct);
 
-router.route('/products/:id').delete(deleteProduct)
+router.route('/products/:id').patch(updateProduct).delete(deleteProduct)
 export const productsRouter = router;
